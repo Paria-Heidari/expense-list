2e0a33f598733214d049bef659b70c56a5339120
Paria <paria.heidari@knowit.no>
2022-07-13 16:16:06 +0200

    created ExpensesFilter component

--- src/components/Expense/Expense.js
+++ src/components/Expense/Expense.js
@@ -1,10 +1,19 @@
 import ExpenseItems from '../ExpenseItems/ExpenseItems';
 import Card from '../card/card';
+import ExpensesFilter from './ExpensesFilter/ExpensesFilter';
 import './Expense.css';
+import { useState } from 'react';
 
 function Expense(props){
+    const [filteredYear, setFilteredYear] = useState('2019');
+    const filterChangeHandler = (selectedYear)=>{
+        setFilteredYear(selectedYear);
+    }
+
+
     return(
         <Card className='expenses'>
+            <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
             <ExpenseItems title={props.title} amount={props.amount} date={props.date}/>
         </Card>
     )
